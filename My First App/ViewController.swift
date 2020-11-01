0ac2e1c88cdc6df9b3cea1e81d1c3de890a5215e
Valery Frenkel <valeron997@gmail.com>
2020-11-01 22:29:26 +0300

    First commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Valery Frenkel on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
