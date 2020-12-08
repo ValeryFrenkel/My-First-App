2439e208d89beb255a69f0dddee3e451fb7a399e
Valery Frenkel <valeron997@gmail.com>
2020-12-08 03:19:04 +0300

    Added URLSessions to News

--- My First App/NewsViewController.swift
+++ My First App/NewsViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 struct Article: Codable {
     
-    let author: String
+    let author: String?
     let title: String
+    
+    
+    private enum CodingKeys: String, CodingKey {
+                case author, title
+            }
+
+    init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            author = try? container.decode(String.self, forKey: .author)
+            title = try container.decode(String.self, forKey: .title)
+        }
+    
+}
+
+struct AllArticles: Codable{
+    
+    let articles: [Article]
+    
 }
 
 
@@ -18,27 +36,57 @@ private let reuseIdentifier = "NewsCell"
 
 class NewsViewController : UICollectionViewController {
     
-    let titlesAndAuthors = [
-        Article(author: "Valery", title: "Covid-19"),
-        Article(author: "Fedor", title: "Covid Vaccine"),
-        Article(author: "Alex", title: "Belarus Elections"),
-        Article(author: "Nikita", title: "Covid Outbreak")
-    ]
-
+    var articles:[Article] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.backgroundColor = .orange
+        
+        guard let newsUrl = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cb2b3fb383dc488eaac69d4704e8ee2d") else { return }
+        
+        let newsSession = URLSession.shared
+        newsSession.dataTask(with: newsUrl) { (data, response, error) in
+            
+            if let response = response {
+              print(response)
+            }
+            
+            guard let data = data else { return }
+            print(data)
+            
+            do {
+                let decoder = JSONDecoder()
+                let a = try decoder.decode(AllArticles.self , from: data)
+                self.articles = a.articles
+                DispatchQueue.main.async {
+                    [self] in
+                    self.collectionView.reloadData()
+                }
+            } catch{
+                print(error)
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [.mutableContainers])
+                let  jsonData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+                print(String(decoding: jsonData, as: UTF8.self))
+            } catch {
+                print(error)
+            }
+        }.resume()
+        
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return titlesAndAuthors.count
+        return articles.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! NewsCell
         
-        cell.titleLable?.text = "\(titlesAndAuthors[indexPath.row].title)"
-        cell.authorLabel?.text = "\(titlesAndAuthors[indexPath.row].author)"
+        cell.titleLable?.text = "\(articles[indexPath.row].title)"
+        cell.authorLabel?.text = "\(articles[indexPath.row].author)"
         
         cell.backgroundColor = .green
         
